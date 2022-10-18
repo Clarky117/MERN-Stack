01ca28fb491e20af30bdc66d211c52f58e2a7fe7
Clarky <clarky117@outlook.com>
2022-10-18 15:18:11 +0800

    server connected to react, can run graphql queries and mutations via apollo dev tool chrome extension

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Testing 123 is this thing on?</h1>
+    </div>
+  );
+}
+
+export default App;
--- server.js
+++ server.js
@@ -26,5 +26,3 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/myfishDB'
     .then(res => {
         console.log(`Server running at ${res.url}`)
     });
-
-
